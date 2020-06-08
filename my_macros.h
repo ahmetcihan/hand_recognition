bd46195fef69ee919516b5a7783494f11cf1d88c
Ahmet Cihan AKINCA <acihana@gmail.com>
2020-06-08 10:59:01 +0300

    120 sets is ok

--- my_macros.h
+++ my_macros.h
@@ -3,12 +3,12 @@
 
 #define INPUT_COUNT     1200
 #define HIDDEN_COUNT_1  128
-#define HIDDEN_COUNT_2  64
-#define HIDDEN_COUNT_3  64
+#define HIDDEN_COUNT_2  128
+#define HIDDEN_COUNT_3  128
 #define HIDDEN_COUNT_4  128
 #define OUTPUT_COUNT    6
 #define IO_ARRAY_LENGTH 6
-#define INPUT_SET       85
+#define INPUT_SET       120
 
 #endif // MY_MACROS
 
