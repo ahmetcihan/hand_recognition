8de5848bb04cc439de73bf4382cc86653cc97270
ahmet cihan <acihana@gmail.com>
2020-07-13 10:36:00 +0300

    neouron counts have been changed

--- my_macros.h
+++ my_macros.h
@@ -2,18 +2,18 @@
 #define MY_MACROS
 
 #define INPUT_COUNT         9*20
-#define HIDDEN_COUNT_1      64
-#define HIDDEN_COUNT_2      64
-#define HIDDEN_COUNT_3      64
+#define HIDDEN_COUNT_1      128
+#define HIDDEN_COUNT_2      128
+#define HIDDEN_COUNT_3      128
 #define HIDDEN_COUNT_4      64
 #define HIDDEN_COUNT_5      64
 #define HIDDEN_COUNT_6      64
 #define HIDDEN_COUNT_7      64
-#define HIDDEN_COUNT_8      64
-#define HIDDEN_COUNT_9      64
-#define HIDDEN_COUNT_10     64
+#define HIDDEN_COUNT_8      32
+#define HIDDEN_COUNT_9      32
+#define HIDDEN_COUNT_10     16
 #define OUTPUT_COUNT        3
-#define INPUT_SET           1000
+#define INPUT_SET           2300
 
 #endif // MY_MACROS
 
